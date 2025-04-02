e650f5c236f22b37ae1c0b4164da4ce344803aff
BoostGo Creators <phucvv@boostgo.com>
2025-04-02 15:46:39 +0700

    add rep function restore, backupo

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ rm Packages.bz2 &&
 dpkg-scanpackages -m ./debs > Packages &&
 bzip2 Packages &&
 git add . &&
-git commit -m "update repo" &&
+git commit -m "add rep function restore, backupo" &&
 git push
