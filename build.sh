7dbebbae5628c4b40e3c79493a9e721d3ab1f9de
BoostGo Creators <phucvv@boostgo.com>
2025-04-03 14:41:54 +0700

    1.1 add wipe

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-rm Packages.bz2 &&
+rm Packages.bz2 && rm Packages &&
 dpkg-scanpackages -m ./debs > Packages &&
 bzip2 Packages &&
 git add . &&
-git commit -m "add function wipe" &&
+git commit -m "1.1 add wipe" &&
 git push
